dbce6b316a900a44135d46d0dace9ccdc2f31a16
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-12-26 08:53:27 -0800

    Tu va

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,82 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Fabric script that distributes an archive to your web servers, using the
+function do_deploy.
 """
 
-from fabric.api import put, run, env
-from os.path import exists
+
+from fabric.api import env, put, run, sudo, local
+from datetime import datetime
+import os
+
+
 env.hosts = ['18.204.13.162', '54.85.22.182']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """Generate a .tgz archive from the contents of web_static."""
+    try:
+        # Create the 'versions' folder if it doesn't exist
+        local("mkdir -p versions")
+
+        # Create the archive filename
+        now = datetime.utcnow()
+        archive_name = "web_static_{}{:02}{:02}{:02}{:02}{:02}.tgz".format(
+            now.year, now.month, now.day, now.hour, now.minute, now.second
+        )
+
+        # Archive the web_static folder
+        local("tar -cvzf versions/{} web_static".format(archive_name))
+
+        # Return the archive path
+        archive_path = os.path.join("versions", archive_name)
+
+        # Get the size of the created archive
+        archive_size = os.path.getsize("versions/{}".format(archive_name))
+
+        print("web_static packed: versions/{} -> {}Bytes".format(
+            archive_name, archive_size
+            ))
+        return archive_path
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive to web servers."""
+
+    if not os.path.exists(archive_path):
         return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-        return False
+
+    # Upload the archive to the /tmp/ directory of the web server
+    put(archive_path, '/tmp/')
+
+    # Extract the archive to /data/web_static/releases/<archive_filename>
+    archive_filename = archive_path.split('/')[-1]
+    release_folder = '/data/web_static/releases/{}'.format(
+        archive_filename.split('.')[0]
+        )
+    run('mkdir -p {}'.format(release_folder))
+    run('tar -xzf /tmp/{} -C {}'.format(
+        archive_filename, release_folder)
+        )
+
+    # Delete the uploaded archive
+    run('sudo rm /tmp/{}'.format(archive_filename))
+
+    # Move files in ./web_static subfolder to parent folder
+    run('cp -r {}/web_static/* {}/'.format(
+        release_folder, release_folder)
+        )
+    run('sudo rm -r {}/web_static'.format(release_folder))
+
+    # Create a new symbolic link to the new version of the code
+    run('sudo rm -rf /data/web_static/current')
+    current_link = '/data/web_static/current'
+    run('sudo ln -sf {} {}'.format(release_folder, current_link))
+
+    print('New version deployed!')
+    return True
