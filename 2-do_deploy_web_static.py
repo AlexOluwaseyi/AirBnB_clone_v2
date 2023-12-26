382038168198b1ec96b6632df9f2939a5600e6cb
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-12-26 09:07:58 -0800

    Revert

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,7 +13,7 @@ import os
 env.hosts = ['18.204.13.162', '54.85.22.182']
 env.user = 'ubuntu'
 
-'''
+
 def do_pack():
     """Generate a .tgz archive from the contents of web_static."""
     try:
@@ -51,82 +51,38 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    # Upload the archive to the /tmp/ directory of the web server
-    put(archive_path, '/tmp/')
-
-    # Extract the archive to /data/web_static/releases/<archive_filename>
-    archive_filename = archive_path.split('/')[-1]
-    release_folder = '/data/web_static/releases/{}'.format(
-        archive_filename.split('.')[0]
-        )
-    run('mkdir -p {}'.format(release_folder))
-    run('tar -xzf /tmp/{} -C {}'.format(
-        archive_filename, release_folder)
-        )
-
-    # Delete the uploaded archive
-    run('sudo rm /tmp/{}'.format(archive_filename))
-
-    # Move files in ./web_static subfolder to parent folder
-    run('cp -r {}/web_static/* {}/'.format(
-        release_folder, release_folder)
-        )
-    run('sudo rm -r {}/web_static'.format(release_folder))
-
-    # Create a new symbolic link to the new version of the code
-    run('sudo rm -rf /data/web_static/current')
-    current_link = '/data/web_static/current'
-    run('sudo ln -sf {} {}'.format(release_folder, current_link))
-
-    print('New version deployed!')
-    return True
-'''
-def do_pack():
-    '''
-    A function that generaees a .tgx archive from the web_static folder
-    '''
-    if not os.path.exists('versions'):
-        os.makedirs('versions')
-
-    filename = time.strftime("%Y%m%d%H%M%S")
-    fullpath = "versions/web_static_{}.tgz".format(filename)
     try:
-        local("tar -cvzf {} web_static".format(fullpath))
-        return fullpath
-    except:
-        return None
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
 
+        # Extract the archive to /data/web_static/releases/<archive_filename>
+        archive_filename = archive_path.split('/')[-1]
+        release_folder = '/data/web_static/releases/{}'.format(
+            archive_filename.split('.')[0]
+            )
+        run('mkdir -p {}'.format(release_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(
+            archive_filename, release_folder)
+            )
 
-def do_deploy(archive_path):
-    '''
-    Distributes an archive to my webservers
-    '''
-    if not os.path.isfile(archive_path):
+        # Delete the uploaded archive
+        run('sudo rm /tmp/{}'.format(archive_filename))
+
+        # Move files in ./web_static subfolder to parent folder
+        run('cp -r {}/web_static/* {}/'.format(
+            release_folder, release_folder)
+            )
+        run('sudo rm -r {}/web_static'.format(release_folder))
+
+        # Create a new symbolic link to the new version of the code
+        run('sudo rm -rf /data/web_static/current')
+        current_link = '/data/web_static/current'
+        run('sudo ln -sf {} {}'.format(release_folder, current_link))
+
+        sudo chown -R ubuntu:ubuntu /data/
+
+        print('New version deployed!')
+        return True
+
+    except Exception as e:
         return False
-
-    # Upload the archive to the server
-    put(archive_path, '/tmp/')
-
-    # Create directory where we will extract the file to
-    file_name = archive_path.split('/')[-1]  # Get file name
-    dir_path = '/data/web_static/releases/{}'.format(file_name.split('.')[0])
-    run('mkdir -p {}'.format(dir_path))
-
-    # Uncompress the archive to the folder
-    server_archive_path = '/tmp/' + file_name  # Path of archive in server
-    run('tar -xzf {} -C {}'.format(server_archive_path, dir_path))
-
-    # Delete the archive from the web server
-    run('rm -rf {}'.format(server_archive_path))
-
-    # Delete the symbolic link
-    run('rm -rf /data/web_static/current')
-
-    # Move the files from web_static to web_static_<number>
-    run('mv {}/web_static/* {}'.format(dir_path, dir_path))
-    run('rm -rf {}/web_static'.format(dir_path))
-
-    # Create a new symbolic link
-    run('ln -s {} /data/web_static/current'.format(dir_path))
-
-    return True
